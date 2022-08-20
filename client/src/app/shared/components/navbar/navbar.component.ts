898e4202854e3030d1993fc58d30dcf2884caa9f
aurelle <he201883@students.ephec.be>
2022-08-20 07:20:35 +0200

    modification du calendrier et resolutions des bug

--- client/src/app/shared/components/navbar/navbar.component.ts
+++ client/src/app/shared/components/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {FlashMessagesService} from 'angular2-flash-messages';
-import {AuthService} from "../../../core/services/auth.service";
+import {AuthService} from "@app/core/services/auth.service";
 import {Router} from "@angular/router";
-import {ValidateService} from "../../../core/services/validate.service";
+import {ValidateService} from "@app/core/services/validate.service";
+import {BehaviorSubject} from "rxjs";
+import {TranslateService} from "@ngx-translate/core";
+import {EToastSeverities, ToastService} from "@app/core/services/toast.service";
 
 @Component({
   selector: 'app-navbar',
@@ -15,8 +17,12 @@ export class NavbarComponent implements OnInit {
   userId: any;
   username: string = "";
 
+  currentLang$: BehaviorSubject<string>;
+  languages = ['en', 'fr'];
+
   constructor(private validateService: ValidateService,
-              private _flashMessagesService: FlashMessagesService,
+              public toastService: ToastService,
+              private readonly translateService: TranslateService,
               public authService: AuthService,
               private router: Router) {
   }
@@ -28,8 +34,24 @@ export class NavbarComponent implements OnInit {
       this.authService.username =
         JSON.parse(localStorage.getItem('user')).username;
     }
+
+    this.translateService.use('en');
   }
 
+  updateLanguage(event) {
+    if (event.checked) {
+      this.translateService.use('de');
+    } else {
+      this.translateService.use('en');
+    }
+  }
+
+  public initLanguage(): void {
+    this.translateService.addLangs(this.languages);
+    const browserLang = this.translateService.getBrowserLang();
+    this.currentLang$.next(browserLang?.match(/en|de/) ? browserLang : 'en');
+    this.translateService.use(this.currentLang$.value);
+  }
 
   onSelect() {
     this.authService.getProfile().subscribe(
@@ -43,22 +65,16 @@ export class NavbarComponent implements OnInit {
     );
   }
 
-  onLogOutClick() {
+  async onLogOutClick() {
 
     if (!Object.keys(localStorage).includes('id_token')) {
       this.authService.logout();
-      this._flashMessagesService.show("Please log in first", {
-        cssClass: "alert-danger w-25",
-        timeout: 2000
-      });
+      this.toastService.show(EToastSeverities.INFO, 'Please log in first');
       return false;
     } else {
       this.authService.logout();
-      this._flashMessagesService.show("You are logged out", {
-        cssClass: "alert-success w-25",
-        timeout: 2000,
-        navigate: `${this.router.navigate(['/login'])}`
-      });
+      this.toastService.show(EToastSeverities.INFO, 'You are logged out !');
+      await this.router.navigate(['/login']);
       return false;
     }
   }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,59 +1,102 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
-import {FlashMessagesModule} from "angular2-flash-messages";
 import {NgxPaginationModule} from 'ngx-pagination';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-import { AddClassDirective } from './directives/add-class.directive';
-import { ValidInputDirective } from './directives/valid-input.directive';
-import { FilterPipe } from './pipes/filter.pipe';
-import { ReplacePipe } from './pipes/replace.pipe';
-import {TimeAgoPipe} from 'time-ago-pipe';
-import { TruncatePipe } from './pipes/truncate.pipe';
-import { FilterLogsPipe } from './pipes/filter-logs.pipe';
-
-import { NavbarComponent } from './components/navbar/navbar.component';
+import {AddClassDirective} from './directives/add-class.directive';
+import {ValidInputDirective} from './directives/valid-input.directive';
+import {FilterPipe} from './pipes/filter.pipe';
+import {ReplacePipe} from './pipes/replace.pipe';
+import {TruncatePipe} from './pipes/truncate.pipe';
+import {FilterLogsPipe} from './pipes/filter-logs.pipe';
+import {ButtonModule} from 'primeng/button';
+import {NavbarComponent} from './components/navbar/navbar.component';
 import {RouterModule} from "@angular/router";
-
+import {TranslateModule} from "@ngx-translate/core";
+import {DividerModule} from 'primeng/divider';
+import {FullCalendarModule} from "primeng/fullcalendar";
+import {ConfirmationDialogComponent} from './components/confirmation-dialog/confirmation-dialog.component';
+import {DialogService} from "primeng/dynamicdialog";
+import {ToastModule} from 'primeng/toast';
+import {TabMenuModule} from 'primeng/tabmenu';
+import {TabViewModule} from 'primeng/tabview';
+import {BadgeModule} from 'primeng/badge';
+import {MessagesModule} from 'primeng/messages';
+import {MessageModule} from 'primeng/message';
+import {RatingModule} from 'primeng/rating';
+import { TagModule } from 'primeng/tag';
+import {SplitButtonModule} from 'primeng/splitbutton';
+import {PanelModule} from 'primeng/panel';
 
 @NgModule({
   declarations: [
     TruncatePipe,
-    TimeAgoPipe,
     ReplacePipe,
     FilterPipe,
     ValidInputDirective,
     AddClassDirective,
     FilterLogsPipe,
-    NavbarComponent
+    NavbarComponent,
+    ConfirmationDialogComponent
   ],
   imports: [
     CommonModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    FlashMessagesModule.forRoot(),
+    FullCalendarModule,
+    PanelModule,
+    ButtonModule,
+    ToastModule,
+    MessagesModule,
+    SplitButtonModule,
+    RatingModule,
+    MessageModule,
+    DividerModule,
+    TagModule,
+    TabMenuModule,
+    TabViewModule,
+    BadgeModule,
     RouterModule,
     NgxPaginationModule,
-    NgbModule
+    NgbModule,
+    TranslateModule,
+    RouterModule,
   ],
   exports: [
     TruncatePipe,
-    TimeAgoPipe,
     ReplacePipe,
     FilterPipe,
     ValidInputDirective,
     AddClassDirective,
     FilterLogsPipe,
+    RatingModule,
+    FullCalendarModule,
+    ButtonModule,
+    SplitButtonModule,
+    PanelModule,
+    TabMenuModule,
+    TagModule,
+    ToastModule,
+    DividerModule,
+    TabViewModule,
+    MessagesModule,
+    MessageModule,
+    BadgeModule,
     ReactiveFormsModule,
+    TranslateModule,
+    ConfirmationDialogComponent,
     FormsModule,
     HttpClientModule,
-    FlashMessagesModule,
     NavbarComponent,
     NgxPaginationModule,
     NgbModule
-  ]
+  ],
+  providers: [
+    DialogService
+  ],
 })
-export class SharedModule { }
+export class SharedModule {
+}
